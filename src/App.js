cb803dbed97ded93deb9f78a56f59db695f4e010
yeonghoon Park <huklmnj2@naver.com>
2022-07-10 01:46:04 +0900

    props 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function MinutesToHours() {
 
   const reset = () => setAmount(0);
 
-  const oninverted = () => {
+  const onInverted = () => {
     reset();
     setInverted((current) => !current);
   }
@@ -38,52 +38,63 @@ function MinutesToHours() {
       />
 
       <button onClick={reset}>Reset</button>
-      <button onClick={oninverted}>{inverted ? 'Convert from minutes to hours' : 'Convert from hours to minutes'}</button>
+      <button onClick={onInverted}>{inverted ? 'Convert from minutes to hours' : 'Convert from hours to minutes'}</button>
     </>
   );
 }
 
 function KmToMiles() {
   const [amount, setAmount] = useState(0);
-  const [flip, setFlip] = useState(false);
+  const [inverted, setInverted] = useState(false);
 
-  const onChange = (event)=>{
+  const onChange = (event) => {
     setAmount(event.target.value);
   }
 
-  const reset = ()=>{
+  const reset = () => {
     setAmount(0);
   }
 
-  const onFlip =()=>{
+  const onInverted = () => {
     reset();
-    setFlip((currentBoolean) => !currentBoolean)
+    setInverted((currentBoolean) => !currentBoolean)
   }
 
   return (
     <>
       <label htmlFor="km">Km</label>
-      <input 
-        type="number" 
+      <input
+        type="number"
         id="km"
-        placeholder="Km" 
-        value={flip ? amount / 1.60934 : amount}
-        disabled={flip}
+        placeholder="Km"
+        value={inverted ? amount * 1.609 : amount}
+        disabled={inverted}
         onChange={onChange}
       />
       <label htmlFor="miles">Miles</label>
-      <input 
-        type="number" 
+      <input
+        type="number"
         id="miles"
-        placeholder="Miles" 
-        value={flip ? amount : amount * 1.60934}
-        disabled={!flip}
+        placeholder="Miles"
+        value={inverted ? amount : amount / 1.609}
+        disabled={!inverted}
         onChange={onChange}
       />
       <button onClick={reset}>Reset</button>
-      <button onClick={onFlip}>{flip ? 'Convert from Km to Miles' : 'Convert from Miles to Km'}</button>
+      <button onClick={onInverted}>{inverted ? 'Convert from Km to Miles' : 'Convert from Miles to Km'}</button>
     </>
   );
+}
+
+function Btn({ text, big }) {
+  return <button style={{
+    backgroundColor: 'tomato',
+    color: '#ffffff',
+    padding: '10px 20px',
+    border: 0,
+    borderRadius: 10,
+    fontSize: big ? '2rem' : '1rem'
+  }}>{text}</button>
 }
 
 function App() {
@@ -105,6 +116,9 @@ function App() {
       <hr />
       {index === '0' ? <MinutesToHours /> : null}
       {index === '1' ? <KmToMiles /> : null}
+      <hr />
+      <Btn text='Save Changes' big={true} />
+      <Btn text='Confirm' big={false} />
     </div>
   );
 }

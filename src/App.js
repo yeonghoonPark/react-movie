073f5d9958c45ceb4b2f293bd54f02b0439641c3
yeonghoon Park <huklmnj2@naver.com>
2022-07-17 00:53:48 +0900

    toDo part2

--- src/App.js
+++ src/App.js
@@ -4,34 +4,46 @@ function App() {
 
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
-  
+
   const onChange = (event) => setToDo(event.target.value);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo === ""){
+    if (toDo === "") {
       return;
     }
-    setToDos((currentToDos) => [toDo, ...currentToDos]);
-    setToDo("");
+    // useState의 set함수에서 함수를 보낼 경우 함수의 첫번째 argument는 현재의 state이다.
+    // {}중괄호가 없을 경우에는 return 생략 가능.
+    setToDos((currentArray) => {
+      return [...currentArray, toDo]
+    });
+    setToDo("")
   }
-    
-  useEffect(()=>{
+
+  useEffect(() => {
     console.log(toDos);
-  },[toDos])
+  }, [toDos])
 
   return (
     <div>
-      <h1>My To Do List({toDos.length})</h1>
+      <h1>To Do List({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
+          type="text"
+          placeholder="Write Your To Do"
           onChange={onChange}
           value={toDo}
-          type="text"
-          placeholder="Write your to do.."
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+      {
+        toDos.map((item, index) => {
+         return <li key={index}>{item.toUpperCase()}</li>;
+        })
+      }
+      </ul>
     </div>
   );
 }
